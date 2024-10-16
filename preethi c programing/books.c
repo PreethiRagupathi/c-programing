4123960c6d1430e5b920b94e958847568d64e29c
PreethiRagupathi <preethirstr@gmail.com>
2024-10-16 22:26:35 +0530

    Delete preethi c programing directory

--- preethi c programing/books.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int x=50525;
-    int y=40399;
-    printf("Total number of copies:%d",x+y);
-    
-}
-
--- preethi c programing/ticket.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int x,y,z;
-    printf("enter x,y,z:");
-    scanf("%d %d %d",&x,&y,&z);
-    printf("The total number of tickets she sold:%d",x+y+z);
-    
-}
