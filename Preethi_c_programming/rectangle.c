44bde4be3b5954db578a65c41106361b3d68eeee
PreethiRagupathi <preethirstr@gmail.com>
2024-11-23 12:09:16 +0530

    Update rectangle.c

--- Preethi_c_programming/rectangle.c
+++ Preethi_c_programming/rectangle.c
@@ -1,3 +1,4 @@
+//c program to find area and perimeter for the rectangle
 #include<stdio.h>
 int main()
 {
@@ -9,4 +10,12 @@ int main()
     printf("area of the square:%d",l*w);
     return 0;
 }
+***********************************************************************
+OUTPUT:
+enter l,w:
+6
+8
+perimetr of the rectangle:28
+area of the square:48
+************************************************************************
 
